6f319cc0e635f2c2c17cc9e16c06e681b10829f0
Anas Ahammad <anasahammad2002@gmail.com>
2024-08-01 12:01:50 +0600

    navbar footer and home banner added

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import Container from "../Container";
+import FooterList from "./FooterList";
+import { MdFacebook } from "react-icons/md";
+import { AiFillInstagram, AiFillTwitterCircle, AiFillYoutube } from "react-icons/ai";
+
+
+const Footer = () => {
+    return (
+        <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+            <Container>
+                <div className="flex flex-col md:flex-row pt-16 pb-8">
+                    <FooterList>
+                        <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+                        <Link href="#">Phones</Link>
+                        <Link href="#">Laptops</Link>
+                        <Link href="#">Desktops</Link>
+                        <Link href="#">Watches</Link>
+                        <Link href="#">TVs</Link>
+                        <Link href="#">Accessories</Link>
+                    </FooterList>
+                    <FooterList>
+                        <h3 className="text-base font-bold mb-2">Customer Services</h3>
+                        <Link href="#">Contact Us</Link>
+                        <Link href="#">Shipping Policy</Link>
+                        <Link href="#">Returns & Exchanges</Link>
+                        <Link href="#">Watches</Link>
+                        <Link href="#">FAQs</Link>
+                      
+                    </FooterList>
+                <div className="w-full md:w-1/3 mb-6 md:mb-0">
+                <h3 className="text-base font-bold mb-2">About Us</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora facilis placeat nam reprehenderit! Distinctio corrupti porro mollitia, laudantium non fugiat expedita aliquid quisquam culpa rerum alias! Porro earum ducimus quis.</p>
+
+                <p>&copy; {new Date().getFullYear()} E~Shop. All Rights Reserve</p>
+                </div>
+
+                <FooterList>
+                <h3 className="text-base font-bold mb-2">Follow Us</h3>
+                <div className="flex gap-3">
+                <Link href="#">
+                    <MdFacebook size={24}/>
+                </Link>
+                <Link href="#">
+                    <AiFillTwitterCircle size={24}/>
+                </Link>
+                <Link href="#">
+                    <AiFillInstagram size={24}/>
+                </Link>
+                <Link href="#">
+                    <AiFillYoutube size={24}/>
+                </Link>
+                </div>
+                </FooterList>
+                </div>
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
