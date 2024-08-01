72c4ebfa8148a0d37b0b92c4905e5928be8d8eda
Anas Ahammad <anasahammad2002@gmail.com>
2024-08-01 23:24:35 +0600

    review section added and moment js added

---
+++ app/components/Heading.tsx
@@ -0,0 +1,16 @@
+
+
+interface HeadingProps{
+    title: any,
+    center?: boolean
+}
+
+const Heading: React.FC<HeadingProps> = ({title, center}) => {
+    return (
+        <div className={`${center ? "text-center": ''}`}>
+            <h1 className="font-bold text-2xl">{title}</h1>
+        </div>
+    );
+};
+
+export default Heading;
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { product } from "@/utils/proudct";
 import ProductDetails from "./ProductDetails";
 import Container from "@/app/components/Container";
+import ListRating from "../ListRating";
 
 interface Iparams{
     productId: string;
@@ -10,7 +11,12 @@ const page = ({params} : {params: Iparams}) => {
     
     return (
         <div className="p-8">
-           <Container> <ProductDetails product={product}/></Container>
+           <Container> <ProductDetails product={product}/>
+            <div className="flex flex-col gap-4 mt-20">
+                <div>Add Rating</div>
+                <ListRating product={product}/>
+            </div>
+           </Container>
         </div>
     );
 };
