a36dc0341207efcdfbbe8791647a84c61169ce8a
Anas Ahammad <anasahammad2002@gmail.com>
2024-08-01 23:38:29 +0600

    avatar added on the review

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
+
+interface AvatarProps{
+    src?: string | null | undefined
+}
+const Avatar: React.FC<AvatarProps> = ({src}) => {
+
+    if(src){
+        return <Image src={src} alt="Avatat" height={30} width={30} className="rounded-full"/>
+    }
+    return <FaUserCircle size={24}/>
+    
+};
+
+export default Avatar;
--- app/product/ListRating.tsx
+++ app/product/ListRating.tsx
@@ -3,6 +3,7 @@
 import moment from "moment";
 import Heading from "../components/Heading";
 import { Rating } from "@mui/material";
+import Avatar from "../components/Avatar";
 
 
 interface ListRatingProps{
@@ -16,7 +17,7 @@ const ListRating: React.FC<ListRatingProps> = ({product}) => {
                 {product.reviews && product.reviews.map((review)=> {
                     return <div key={review.id} className="max-w-[300px] ">
                         <div className="flex gap-2 items-center">
-                            <div>Avatar</div>
+                            <Avatar src={review.user.image}/>
                             <div className="font-semibold">{review.user.name}</div>
                             <div className="font-light">{moment(review.createdDate).fromNow()}</div>
 
