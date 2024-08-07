f97ae6f82c7353c686ede02150da46ac183e78c3
Anas Ahammad <anasahammad2002@gmail.com>
2024-08-08 00:26:04 +0600

    basic prisma structure added for next auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,65 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import Google from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import prisma from "@/libs/prismadb";
+import bcrypt from "bcrypt"
+ 
+export default  NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    }),
+    CredentialsProvider({
+        name: "credentials",
+        credentials : {
+            email : {
+                label: 'email',
+                type: 'email'
+            },
+            password : {
+                label: 'password',
+                type: 'password'
+            }
+        },
+        async authorize(credentials){
+            if(!credentials?.email || !credentials.password) {
+                throw new Error('Invalid Email or Password')
+            }
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    email : credentials.email
+                }
+            })
+
+            if(!user || !user?.hashedPassword){
+                throw new Error('Invalid Email or Password')
+            }
+
+            const isCorrectPassword = await bcrypt.compare(
+                credentials.password,
+                user.hashedPassword
+            )
+
+            if(!isCorrectPassword){
+                throw new Error('Invalid  Password')
+            }
+
+            return user;
+        }
+    })
+  ],
+
+  pages : {
+    signIn : "/login"
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session : {
+    strategy : 'jwt'
+  },
+  secret : process.env.NEXTAUTH_SECRET
+})
