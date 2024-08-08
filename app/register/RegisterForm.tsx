4ec714c9c0e1c0ebcd06dc7bc0a56a7011165b17
Anas Ahammad <anasahammad2002@gmail.com>
2024-08-08 15:58:52 +0600

    register api added and connected to the mongodb

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,10 +7,15 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 
 const RegisterForm = () => {
     const [isLoading, setIsLoading] = useState(false)
+    const router = useRouter()
     const {register, handleSubmit, formState:{errors}} = useForm<FieldValues>({
         name: "",
         email : "",
@@ -19,7 +24,27 @@ const RegisterForm = () => {
 
     const onSubmit: SubmitHandler<FieldValues> = (data)=>{
         setIsLoading(true)
-        console.log(data)
+        axios.post('/api/register', data).then(()=>{
+            toast.success("Account Crated Successfully")
+
+            signIn('credentials', {
+                email: data.email,
+                password: data.password,
+                redirect: false
+            }).then((callback)=>{
+                if(callback?.ok){
+                    router.push('/cart')
+                    router.refresh();
+                    toast.success("Logged In")
+                    
+                }
+                if(callback?.error){
+                    toast.error(callback.error)
+                }
+            })
+        }).catch(()=> toast.error("Something went wrong!")).finally(()=>  setIsLoading(false))
+          
+       
     }
     return (
         <>
